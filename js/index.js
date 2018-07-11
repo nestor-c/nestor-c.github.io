f987e0102f45288ac31bfaa37ac6d2b7306f884d
Nestor <Nestord.cortes@gmail.com>
2018-07-11 07:57:31 -0700

    added a navbar, and a way to scale content

---
+++ js/index.js
@@ -0,0 +1,3 @@
+var myNameIs = document.getElementById('KnowMe');
+myNameIs.addEventListener("mouseover", function(){myNameIs.style.fontSize = ".5em"});
+myNameIs.addEventListener("mouseout", function(){myNameIs.style.fontSize= ".35em" });
