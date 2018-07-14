ba3d5c0e396b708e97f477431f2973b0f1b08bda
Nestor <Nestord.cortes@gmail.com>
2018-07-13 19:28:05 -0700

    trying the push again

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 var myNameIs = document.getElementById('KnowMe');
-myNameIs.addEventListener("mouseover", function(){myNameIs.style.fontSize = ".5em"});
-myNameIs.addEventListener("mouseout", function(){myNameIs.style.fontSize= ".35em" });
+myNameIs.addEventListener("mouseover", function(){
+  myNameIs.style.fontSize = ".5em";
+});
+myNameIs.addEventListener("mouseout", function(){
+  myNameIs.style.fontSize= ".35em"
+});
